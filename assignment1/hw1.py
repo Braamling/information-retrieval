335d8635ccbb31300e3b81b0227e29a8f83ba47d
Haitam <haitam_benyahia@hotmail.com>
2017-01-10 12:09:21 +0100

    yield function

--- assignment1/hw1.py
+++ assignment1/hw1.py
@@ -3,9 +3,17 @@ import itertools
 def retrieve_pairs():
 	return [(x[:5], x[5:]) for x in itertools.product((0, 1, 2), repeat=10)]
 
+def retrieve_pair():
+    for x in itertools.product((0, 1, 2), repeat=10):
+        yield (x[:5], x[5:])
 
 def main():
-	print retrieve_pairs()
+    pair = retrieve_pair()
+
+    for i in range(10):
+        print pair.next()
+
+
 
 if __name__ == '__main__':
 	main()
