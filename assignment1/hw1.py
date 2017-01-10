65dcd6807d0f3c1f8f6ac9d16b35c40009df189b
Haitam <haitam_benyahia@hotmail.com>
2017-01-10 13:25:49 +0100

    recall and precision function

--- assignment1/hw1.py
+++ assignment1/hw1.py
@@ -4,9 +4,36 @@ import math
 def retrieve_new_pairs():
 	return [(x[:5], x[5:]) for x in itertools.product((0, 1, 2), repeat=10)]
 
+# This function returns an iterator which returns a unique ranking of P and E for each iteration
 def retrieve_pair():
     for x in itertools.product((0, 1, 2), repeat=10):
         yield (x[:5], x[5:])
+
+def calc_recall(ranking, n_rel_docs, k=5):
+    counter = 0
+    for rel in ranking[:k]:
+        if rel >= 1:
+            counter+=1
+    return float(counter)/n_rel_docs
+
+# This function calculates the precision at rank k, k needs to be smaller than the ranking size.
+def calc_precision(ranking, k=5):
+    assert k<len(ranking), "k needs to be smaller than the amount of documents"
+    counter = 0
+    for rank, rel in enumerate(ranking[:k], i):
+        if rel >=1:
+            counter += 1
+    return counter/k
+
+
+def calc_avg_precision(ranking, n_rel_docs):
+    avg_prec = 0
+    counter = 0
+    for rank, rel in enumerate(ranking,1):
+        if rel >= 1:
+            counter+=1
+            avg_prec += float(counter)/rank
+    return avg_prec/n_rel_docs
 
 def calc_dcg(ranking, k=5):
 	dcg = 0
@@ -15,19 +42,16 @@ def calc_dcg(ranking, k=5):
 
 	return dcg
 
-def avg_precision(p, n_rel_docs):
-    avg_prec = 0
-    counter = 0
-    for i, rel in enumerate(p,1):
-        if rel >= 1:
-            counter+=1
-            avg_prec += float(counter)/i
-    return avg_prec/n_rel_docs
+def calc_RBP(ranking, theta=0.8):
+    RBP = 0
+    for rank, rel in enumerate(ranking,1):
+        RBP += rel * theta**(rank-1) * (1-theta)
+    return RBP
 
 def main():
     pair = retrieve_pair()
 
-    print avg_precision([1,0,2,1,0,1], 10)
+    print calc_avg_precision([1,0,2,1,0,1], 10)
     for i in range(10):
         print pair.next()
 
