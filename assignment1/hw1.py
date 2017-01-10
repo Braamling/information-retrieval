ef99543191b4457549b9bb0eb6603772d8a1ea80
Bram van den Akker <git@bramvandenakker.nl>
2017-01-10 13:29:51 +0100

    measuremest

--- assignment1/hw1.py
+++ assignment1/hw1.py
@@ -1,7 +1,7 @@
 import itertools
 import math
 
-def retrieve_new_pairs():
+def retrieve_pairs():
 	return [(x[:5], x[5:]) for x in itertools.product((0, 1, 2), repeat=10)]
 
 # This function returns an iterator which returns a unique ranking of P and E for each iteration
@@ -37,8 +37,10 @@ def calc_avg_precision(ranking, n_rel_docs):
 
 def calc_dcg(ranking, k=5):
 	dcg = 0
-	for rank, rel in enumerate(ranking[:k]):
-		dcg += (2**rel - 1) / math.log(2 + rank, 2)
+	# Iterate over each position in the ranking until at position k
+	for rank, rel in enumerate(ranking[:k], 1):
+		# Calculate the dg of the current rank an dadd it to the sum of the dcg.
+		dcg += (2**rel - 1) / math.log(1 + rank, 2)
 
 	return dcg
 
@@ -48,14 +50,67 @@ def calc_RBP(ranking, theta=0.8):
         RBP += rel * theta**(rank-1) * (1-theta)
     return RBP
 
+def normalized_dcg(ranking, k=5):
+	# Calculate the unnormalized dcg of the ranking up to point k
+	dcg = calc_dcg(ranking, k)
+
+	# Create the best ranking and calculate its dcg over the whole ranking.
+	best_rank = sorted(ranking, reverse=True) 
+	best_dcg = calc_dcg(best_rank, len(best_rank))
+
+	# Prevent devision by zero
+	if best_dcg == 0.0:
+		return 1.0
+
+	# Normalize the dcg
+	return dcg/best_dcg
+
+"""
+Calculate all implemented scores for a specific ranking
+"""
+def calculate_scores(ranking):
+	results = {}
+	results['dcg'] = calc_dcg(ranking, len(ranking))
+	results['ndcg'] = normalized_dcg(ranking, len(ranking))
+	results['rbp'] = calc_RBP(ranking)
+	results['ndcg'] = normalized_dcg(ranking, len(ranking))
+	results['recall'] = calc_recall(ranking, 5, len(ranking))
+	results['precision'] = calc_precision(ranking, len(ranking))
+	results['avg_precision'] = calc_avg_precision(ranking, 5)
+
+	return results
+
+def calculate_measure(pair):
+	# ITerate over all scoring types and calculate the measurements
+	measures = ['dcg', 'ndcg', 'rbp', 'ndcg', 'recall', 'precision', 'avg_precision']
+
+	# Calculate both scores
+	results_p = calculate_scores(pair[0])
+	results_e = calculate_scores(pair[1])
+
+	results = {}
+
+	for measurement in measures:
+		# If any of the measurements is negative, the results are discarded. 
+		result = results_p[measurement] - results_e[measurement]
+		if result < 0:
+			return None
+
+		# Add the measurement to the results
+		results[measurement] = result
+
+	return results
+
 def main():
-    pair = retrieve_pair()
+    pairs = retrieve_pairs()
 
-    print calc_avg_precision([1,0,2,1,0,1], 10)
-    for i in range(10):
-        print pair.next()
+    # Check the measurements for each pair
+    for pair in pairs:
+    	measure = calculate_measure(pair)
 
-    new_pair = pair.next()
+    	# Ignore the pair if result_e < result_p
+    	if measure is not None:
+	    	print measure
 
 
 if __name__ == '__main__':
